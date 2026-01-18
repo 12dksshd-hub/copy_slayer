4074b40dd1d7d89ab5cf11cb9d7466d0160964f3
12dksshd-hub <12dksshd@gmail.com>
2026-01-19 01:35:52 +0900

    로딩 완료 텍스트 추가
    로딩 화면에서 완료 전까지 넘어가지 않도록 수정
    로딩 완료 텍스트 추가 및 완료 전까지 보이지 않도록 구현

--- Assets/Script/SceneLoader/SceneLoader.cs
+++ Assets/Script/SceneLoader/SceneLoader.cs
@@ -1,15 +1,22 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    AsyncOperation asyncOperation;
+    private AsyncOperation asyncOperation;
+
+    [SerializeField]
+    private TextMeshProUGUI loadingText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         asyncOperation = SceneManager.LoadSceneAsync(1);
         asyncOperation.allowSceneActivation = false;
+
+        StartCoroutine(ShowLoadCompleteText());
     }
 
     // Update is called once per frame
@@ -20,6 +27,15 @@ public class SceneLoader : MonoBehaviour
 
     public void OnTouch()
     {
-        asyncOperation.allowSceneActivation = true;
+        if(asyncOperation.progress >= 0.9f)
+            asyncOperation.allowSceneActivation = true;
+    }
+
+    private IEnumerator ShowLoadCompleteText()
+    {
+        while(asyncOperation.progress < 0.9f)
+            yield return null;
+
+        loadingText.gameObject.SetActive(true);
     }
 }
